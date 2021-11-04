2fa28012f5993fa4bb2e45425c3d4f95942bb23f
Israel Moreno <se.israel.mc@gmail.com>
2021-11-03 18:01:35 -0600

    Feat/typescript eslint (#1)
    * Configuracion de typescript + eslint
    
    * remove rules on eslint

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../styles/index.css";
+// markup
+const IndexPage: React.FC = () => {
+  return (
+    <main>
+      <div className="container-building">Hello</div>
+    </main>
+  );
+};
+
+export default IndexPage;
