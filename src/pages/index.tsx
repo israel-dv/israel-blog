a70a2f50c59ef840cab1de34e53303459cbcb8df
Israel Moreno <se.israel.mc@gmail.com>
2021-11-03 18:16:22 -0600

    Feat/tailwind configuration (#2)
    * Feat/typescript eslint (#1)
    
    * Configuracion de typescript + eslint
    
    * remove rules on eslint
    
    * installation and configuration tailwindcss

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../styles/index.css";
+// markup
+const IndexPage: React.FC = () => {
+  return (
+    <main>
+      <div className="container-building bg-red-500">Hello</div>
+    </main>
+  );
+};
+
+export default IndexPage;
