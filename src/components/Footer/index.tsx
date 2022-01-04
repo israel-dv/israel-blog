3d4a9493b74419da5facec4d71aacc7ae8571ef3
israel-dv <se.israel.mc@gmail.com>
2022-01-04 16:26:21 -0600

    Firt implentation of footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="fixed w-full h-36 bg-blue-1000 bottom-0 shadow-xl flex flex-col items-center justify-center text-gray-300">
+      <span>Israel Moreno | Todos los derechos reservados</span>
+      <span>Github | Instagram | Twitter</span>
+    </footer>
+  );
+};
+
+export default Footer;
